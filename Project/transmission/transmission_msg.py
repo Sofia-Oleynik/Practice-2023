e68a5e84995611c54874dc83036e5c5d5c4f3c76
Sofia-Oleynik <113835759+Sofia-Oleynik@users.noreply.github.com>
2023-07-19 21:29:35 +0300

    Update transmission_msg.py

--- Project/transmission/transmission_msg.py
+++ Project/transmission/transmission_msg.py
@@ -8,12 +8,12 @@ port_m = 54321 # порт для сообщений
 s_m.connect((host, port_m))
 
 while True:
-    """
+    
     msg_data = s_m.recv(1024)
     message = msg_data.decode()
     if message:
         print('Received message:', message)
-    """
+    
 
 # Закрываем соединение и окна OpenCV
 s_m.close()
