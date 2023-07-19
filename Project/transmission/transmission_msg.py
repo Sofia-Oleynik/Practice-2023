f2ed287d27d76310ee55519e4f9ec5e8b05e4ddc
Sofia-Oleynik <113835759+Sofia-Oleynik@users.noreply.github.com>
2023-07-19 21:29:08 +0300

    Update and rename transmission_msg to transmission_msg.py

---
+++ Project/transmission/transmission_msg.py
@@ -0,0 +1,20 @@
+import socket
+
+s_m = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = 'localhost'
+port_m = 54321 # порт для сообщений
+
+s_m.connect((host, port_m))
+
+while True:
+    """
+    msg_data = s_m.recv(1024)
+    message = msg_data.decode()
+    if message:
+        print('Received message:', message)
+    """
+
+# Закрываем соединение и окна OpenCV
+s_m.close()
+
