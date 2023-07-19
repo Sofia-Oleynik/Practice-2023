663277b2d2f1ad9e8869cf8749ad84a560f95651
Sofia-Oleynik <113835759+Sofia-Oleynik@users.noreply.github.com>
2023-07-19 21:21:41 +0300

    Update skeleton_recognition.py

--- Project/skeleton_recognition/skeleton_recognition.py
+++ Project/skeleton_recognition/skeleton_recognition.py
@@ -1,28 +1,73 @@
 #get_ipython().system('pip install mediapipe==0.10.2 opencv-python')
 
 """
+Расшифровка кодов в сообщениях
+
 1. Контроль головы:
- 1.1. Мочка уха и средняя точка плечевого пояса должны лежать на одной прямой - mistake_code 1.1
- 1.2. Мочка уха и нос должны лежать на одной прямой - code 1.2
-2. Контроль рук (точки плеча, локтя, кисти), угол в пределах 80-100 градусов - mistake_code 2
-3. Контроль длины (длина спины, средняя точка плечевого пояса, тазового пояса, колена)
-4. Контроль ног (крайняя точка тазового пояса, колена и лодыжки), угол в пределах 80-100 градусов - mistake_code 4
+- code 11 - 1.1. Мочка уха и средняя точка плечевого пояса должны лежать на одной прямой
+- code 12 - 1.2. Мочка уха и нос должны лежать на одной прямой
+- code 2 - 2. Контроль рук (точки плеча, локтя, кисти), угол в пределах 80-100 градусов - code 2 - исключено из программы!
+3. Контроль длины
+- code 31 - 1. Длина спины (сутулость)
+- code 32 - 2. Средняя точка плечевого пояса, тазового пояса, колена, угол в пределах 80-100 градусов
+- code 4 - 4. Контроль ног (крайняя точка тазового пояса, колена и лодыжки), угол в пределах 80-100 градусов
+
+code 0 - нужно сутулиться (для контроля сутулости)
+code 1 - нужно выпрямиться
 
 """
 
+import time
+import math
+import socket
+import struct
+import pickle
 import cv2
 import numpy as np
 import mediapipe as mp
+
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
+
+s_v = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s_m = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = 'localhost'
+port_v = 12345
+port_m = 54321
+
+s_v.bind((host, port_v))
+s_m.bind((host, port_m))
+s_v.listen(1)
+s_m.listen(1)
+conn_v, addr_v = s_v.accept()
+conn_m, addr_m = s_m.accept()
 
 delt_code_1_1 = 0.05
 delt_code_1_2 = 0.02
 delt_angle_max = 100
 delt_angle_min = 80
-delt_code_3_1 = 0.05
-delt_code_3_2 = 0.05
+delt_code_3_1 = 0.03
 
+flag_spine = False
+
+time_11 = 0
+time_12 = 0
+time_31 = 0
+time_32 = 0
+time_4 = 0
+
+waiting_time = 600
+
+min_spine_length = 0.0
+
+code_11 = '11'
+code_12 = '12'
+code_31 = '31'
+code_32 = '32'
+code_4 = '4'
+code_0 = '0'
+code_1 = '1'
 
 def calculate_angle(a, b, c):
     a = np.array(a) # первая точка
@@ -38,6 +83,12 @@ def calculate_angle(a, b, c):
     return angle 
 
 cap = cv2.VideoCapture(0)
+
+"""
+# Устанавливаем размер кадра
+cap.set(3, 640)
+cap.set(4, 480)
+"""
 
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose: # установка порогов доверия
     while cap.isOpened():
@@ -91,6 +142,45 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             cv2.line(image, (int(avg_point_shoulder[0] * frame.shape[1]), int(avg_point_shoulder[1] * frame.shape[0])),
                      (int(avg_point_hip[0] * frame.shape[1]), int(avg_point_hip[1] * frame.shape[0])), (255, 0, 0), 2)
 
+            # ---------------------------------------------------------------------------------3 контроль спины
+            # определение длины позвоночника при сутулости
+
+            while flag_spine != True:
+                local_flag = True
+                if local_flag:
+                    conn_m.send(code_0.encode())
+                    time.sleep(7)
+                    local_flag = False
+                min_spine_length = math.sqrt(
+                    (avg_point_shoulder[0] - avg_point_hip[0]) ** 2 + (avg_point_shoulder[1] - avg_point_hip[1]) ** 2)
+                conn_m.send(code_1.encode())
+                flag_spine = True
+            """
+            cv2.putText(image, str(abs(math.sqrt((avg_point_shoulder[0] - avg_point_hip[0])**2 + (avg_point_shoulder[1] - avg_point_hip[1])**2) - min_spine_length)),
+                        tuple(np.multiply(avg_point_shoulder, [640, 480]).astype(int)),
+                        # выводим рядом с нужной точкой в координатах для изображения
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA  # устанавливаем шрифт
+                        )
+            """
+            if abs(math.sqrt((avg_point_shoulder[0] - avg_point_hip[0]) ** 2 + (avg_point_shoulder[1] - avg_point_hip[1]) ** 2) - min_spine_length) < delt_code_3_1:
+                time_31 += 1
+                if time_31 > waiting_time:
+                    conn_m.send(code_31.encode())
+                    time_31 = 0
+            else:
+                time_31 = 0
+            # считаем угол между туловищем и ногами
+            angle_l_spine = calculate_angle(left_shoulder, left_hip, left_knee)
+            angle_r_spine = calculate_angle(right_shoulder, right_hip, right_knee)
+
+            if angle_l_spine > delt_angle_max or angle_l_spine < delt_angle_min or angle_r_spine > delt_angle_max or angle_r_spine < delt_angle_min:
+                time_32 += 1
+                if time_32 > waiting_time:
+                    conn_m.send(code_32.encode())
+                    time_32 = 0
+            else:
+                time_32 = 0
+
             # ---------------------------------------------------------------------------------1 контроль головы
             # приблизительно одна прямая уха и средней точки плечевого пояса
             """
@@ -106,7 +196,12 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                         )
             """
             if abs(avg_point_shoulder[0] - right_ear[0]) > delt_code_1_1 or abs(avg_point_shoulder[0] - left_ear[0]) > delt_code_1_1:
-                print('mistake_code 1.1')
+                time_11 += 1
+                if time_11 > waiting_time:
+                    conn_m.send(code_11.encode())
+                    time_11 = 0
+            else:
+                time_11 = 0
 
             # приблизительно одна прямая уха и носа
 
@@ -124,16 +219,21 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             """
 
             if abs(nose[1] - right_ear[1]) > delt_code_1_2 or abs(nose[1] - left_ear[1]) > delt_code_1_2:
-                print('mistake_code 1.2')
+                time_12 += 1
+                if time_12 > waiting_time:
+                    conn_m.send(code_12.encode())
+                    time_12 = 0
+            else:
+                time_12 = 0
 
             # ---------------------------------------------------------------------------------2 контроль рук
-
+            """
             # считаем угол по трем точкам
             angle_l_hand = calculate_angle(left_shoulder, left_elbow, left_wrist)
             angle_r_hand = calculate_angle(right_shoulder, right_elbow, right_wrist)
             
             # выводим угол в поток изображения
-            """
+            
             cv2.putText(image, str(angle_l),
                            tuple(np.multiply(left_elbow, [640, 480]).astype(int)), # выводим рядом с нужной точкой в координатах для изображения
                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA # устанавливаем шрифт
@@ -143,30 +243,39 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                         # выводим рядом с нужной точкой в координатах для изображения
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA  # устанавливаем шрифт
                         )
-            """
+            
             if angle_l_hand > delt_angle_max or angle_l_hand < delt_angle_min or angle_r_hand > delt_angle_max or angle_r_hand < delt_angle_min:
                 print('mistake_code 2')
+            """
 
-                # ---------------------------------------------------------------------------------4 контроль ног
 
-                # считаем угол по трем точкам
-                angle_l_foot = calculate_angle(left_hip, left_knee, left_ankle)
-                angle_r_foot = calculate_angle(right_hip, right_knee, right_ankle)
+            # ---------------------------------------------------------------------------------4 контроль ног
 
-                # выводим угол в поток изображения
-                """
-                cv2.putText(image, str(angle_l),
-                               tuple(np.multiply(left_elbow, [640, 480]).astype(int)), # выводим рядом с нужной точкой в координатах для изображения
-                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA # устанавливаем шрифт
-                                    )
-                cv2.putText(image, str(angle_r),
-                            tuple(np.multiply(right_elbow, [640, 480]).astype(int)),
-                            # выводим рядом с нужной точкой в координатах для изображения
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA  # устанавливаем шрифт
-                            )
-                """
-                if angle_l_foot > delt_angle_max or angle_l_foot < delt_angle_min or angle_r_foot > delt_angle_max or angle_r_foot < delt_angle_min:
-                    print('mistake_code 4')
+            # считаем угол по трем точкам
+            angle_l_foot = calculate_angle(left_hip, left_knee, left_ankle)
+            angle_r_foot = calculate_angle(right_hip, right_knee, right_ankle)
+
+            # выводим угол в поток изображения
+            """
+            cv2.putText(image, str(angle_l),
+                        tuple(np.multiply(left_elbow, [640, 480]).astype(int)),
+                        # выводим рядом с нужной точкой в координатах для изображения
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA  # устанавливаем шрифт
+                        )
+            cv2.putText(image, str(angle_r),
+                        tuple(np.multiply(right_elbow, [640, 480]).astype(int)),
+                        # выводим рядом с нужной точкой в координатах для изображения
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA  # устанавливаем шрифт
+                        )
+                
+            """
+            if angle_l_foot > delt_angle_max or angle_l_foot < delt_angle_min or angle_r_foot > delt_angle_max or angle_r_foot < delt_angle_min:
+                time_4 += 1
+                if time_4 > waiting_time:
+                    conn_m.send(code_4.encode())
+                    time_4 = 0
+            else:
+                time_4 = 0
                         
 
         except: # если у нас есть не все точки или появилась какая-то ошибка, то мы не разрываем цикл, а просто его пропускаем
@@ -181,8 +290,16 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
         cv2.imshow('Mediapipe Feed', image)
 
+        data = pickle.dumps(image)
+        size = struct.pack('L', len(data))
+        conn_v.sendall(size + data)
+
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
+    conn_v.close()
+    conn_m.close()
+    s_v.close()
+    s_m.close()
     cap.release()
     cv2.destroyAllWindows()
